7e66704f5facf9d1da9a7dfb1784967876f2a338
dillonthompson <dthompson@virtru.com>
2022-11-09 18:04:11 -0600

    build app and update readme

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # luminai-browser-extension
+
+## Steps to run application
+1. Make sure you're using node 14 or above and run `npm ci` to install deps
+2. run `npm start`
+3. open a chrome browser and navigate to the extensions page: `chrome://extensions/`
+4. make sure the developer mode switch is turned on in the top right corner
+5. click the `load unpacked` button in the top left corner
+6. navigate to the `build` folder in the root directory of this project and select that folder to load
+7. open a new tab and click on the new browser extension in the top right corner of your browser
+8. click the `RUN` button
+9. watch the app work
--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,6 +1,32 @@
-import { printLine } from './modules/print';
+const marketUrl = 'https://www.cnbc.com/us-market-movers/';
+const formUrl = 'https://forms.zohopublic.in/developers/form/TestResponses/formperma/-gq-UT1RjqASnGgBsW-M8MmPm8e3YLhcyFam06v2piE';
+const marketData = {};
 
-console.log('Content script works!');
-console.log('Must reload extension for modifications to take effect.');
+window.onload = async () => {
+  if (window.location.href === marketUrl) {
+    const markets = document.getElementsByClassName('MarketMoversMenu-marketOption');
+    let tableRow;
+    setTimeout(() => {
+      markets[1].click();
+      setTimeout(() => {
+        tableRow = document.querySelector('#MainContentContainer > div > div > div > div:nth-child(3) > div.PageBuilder-col-9.PageBuilder-col > section > section.MarketMovers-marketTopContainer.MarketMovers-marketTopContainerStacked > section:nth-child(1) > table > tbody > tr:nth-child(2)');
+        marketData.name = tableRow.children[1].textContent;
+        marketData.change = tableRow.children[3].textContent.replace('%', '');
+        marketData.timestamp = Date.now();
+        chrome.storage.sync.set({ marketData });
+        setTimeout(() => {
+          window.location.href = formUrl;
+        }, 500);
+      }, 500);
+    }, 250);
+  }
+  if (window.location.href === formUrl) {
+    let formData;
+    await chrome.storage.sync.get('marketData').then(d => formData = d.marketData);
 
-printLine("Using the 'printLine' function from the Print Module");
+    document.getElementsByName('SingleLine')[0].value = formData.name;
+    document.getElementsByName('SingleLine1')[0].value = formData.change;
+    document.getElementsByName('SingleLine2')[0].value = formData.timestamp;
+    console.log(formData.name, formData.change, formData.timestamp);
+  }
+}
